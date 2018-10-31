33d4d30431e32db16629651551de620f0f5469a1
Denis Wirries <denis@wirries.me>
2018-10-31 18:04:06 +0100

    Rename

--- coffeeservice/src/main/java/me/wirrries/coffeemonitor/coffeeservice/CoffeeServiceApplication2.java
+++ coffeeservice/src/main/java/me/wirrries/coffeemonitor/coffeeservice/CoffeeServiceApplication2.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoffeeserviceApplication {
+public class CoffeeServiceApplication2 {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoffeeserviceApplication.class, args);
+        SpringApplication.run(CoffeeServiceApplication2.class, args);
     }
 }
