ee99e6cc04ba9e09615f7bc3c2943c2651f3981f
Denis Wirries <denis@wirries.me>
2018-10-31 18:02:13 +0100

    Empty application

---
+++ coffeeservice/src/main/java/me/wirrries/coffeemonitor/coffeeservice/CoffeeserviceApplication.java
@@ -0,0 +1,12 @@
+package me.wirrries.coffeemonitor.coffeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeserviceApplication.class, args);
+    }
+}
