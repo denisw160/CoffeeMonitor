df9644760002ba48d4c2335316e6febcad28a76b
Denis Wirries <denis@wirries.me>
2018-11-02 15:25:12 +0100

    Reading data from rest and update the status

--- coffeeapp/src/app/api.service.ts
+++ coffeeapp/src/app/api.service.ts
@@ -1,7 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
+
+import {SensorData} from './sensordata';
+import {Alive} from './alive';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +14,29 @@ export class ApiService {
   constructor(private _http: HttpClient) {
   }
 
-  getData(): Observable<any> {
-    return this._http.get('/api/data');
+  // Status Services
+
+  /**
+   * Query the service, if the sensor is alive.
+   */
+  getAlive(): Observable<Alive> {
+    return this._http.get<Alive>('/api/alive');
   }
 
-  getDataLatest(): Observable<any> {
-    return this._http.get('/api/data/latest');
+  // Data Services
+
+  /**
+   * Get all data directly from the database.
+   */
+  getData(): Observable<SensorData[]> {
+    return this._http.get<SensorData[]>('/api/data');
+  }
+
+  /**
+   * Get the latest data entry from the database.
+   */
+  getDataLatest(): Observable<SensorData> {
+    return this._http.get<SensorData>('/api/data/latest');
   }
 
   // TODO Add service for configuration
--- coffeeapp/src/app/dashboard/dashboard.component.ts
+++ coffeeapp/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,63 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {timer} from 'rxjs';
+import {ApiService} from '../api.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  @ViewChild('coffee-status') imgCoffeeStatus: ElementRef;
+
+  updateTimer = timer(100, 5000);
+  updateSubscription;
+
+  constructor(private _api: ApiService) {
+  }
+
+  /**
+   * Update the icon for the coffee machine. Shows of the sensors.
+   */
+  updateAliveStatus() {
+    const alive = this._api.getAlive();
+    alive.subscribe(a => {
+      if (a.alive) {
+        document.getElementById('coffee-status').setAttribute('src', '../../assets/status-info.png');
+      } else {
+        document.getElementById('coffee-status').setAttribute('src', '../../assets/status-warning.png');
+      }
+    });
+  }
+
+  /**
+   * Updates the status for the fill level, allocation from the sensor data.
+   */
+  updateFillAllocation() {
+    const currentData = this._api.getDataLatest();
+    currentData.subscribe(r => {
+      console.log('ID: ' + r.id);
+      console.log('TS: ' + r.timestamp);
+      console.log('Weight: ' + r.weight);
+      console.log('Allocated: ' + r.allocated);
+    });
+
+  }
+
+  updateDashboard(t) {
+    console.log('Update data: ' + t + ' - ' + new Date()); // TODO REMOVE
+
+    this.updateAliveStatus();
+    this.updateFillAllocation();
   }
 
   ngOnInit() {
+    this.updateSubscription = this.updateTimer.subscribe(t => this.updateDashboard(t));
+  }
+
+  ngOnDestroy() {
+    this.updateSubscription.unsubscribe();
   }
 
 }
