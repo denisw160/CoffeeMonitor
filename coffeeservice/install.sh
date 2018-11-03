d3141f6099b6f94ec5245be8f58eae03fe9143ac
Denis Wirries <denis@wirries.me>
2018-11-03 17:26:19 +0100

    optimize scripts

--- coffeeservice/install.sh
+++ coffeeservice/install.sh
@@ -4,9 +4,11 @@
 #
 
 APP_DIR=$(pwd -P)
+PROCESS_USR=$(whoami)
 
 echo Installing CoffeeService ...
 sudo cp src/service/coffeeservice.service  /etc/systemd/system/coffeeservice.service
 sudo sed -i -e "s~##APP_DIR##~$APP_DIR~g" /etc/systemd/system/coffeeservice.service
+sudo sed -i -e "s~##PROCESS_USR##~$PROCESS_USR~g" /etc/systemd/system/coffeeservice.service
 sudo systemctl enable coffeeservice.service
 sudo systemctl start coffeeservice.service
