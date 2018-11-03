8b2f460a35ddd9bf6ae7f7d7f6b0613b714bc6d3
Denis Wirries <denis@wirries.me>
2018-11-03 17:25:11 +0100

    optimize script

--- coffeesensor/install.sh
+++ coffeesensor/install.sh
@@ -4,9 +4,11 @@
 #
 
 APP_DIR=$(pwd -P)
+PROCESS_USR=$(whoami)
 
 echo Installing CoffeeSensor ...
-sudo cp src/coffeesensor.service  /etc/systemd/system/coffeesensor.service
+sudo cp service/coffeesensor.service  /etc/systemd/system/coffeesensor.service
 sudo sed -i -e "s~##APP_DIR##~$APP_DIR~g" /etc/systemd/system/coffeesensor.service
+sudo sed -i -e "s~##PROCESS_USR##~$PROCESS_USR~g" /etc/systemd/system/coffeesensor.service
 sudo systemctl enable coffeesensor.service
 sudo systemctl start coffeesensor.service
