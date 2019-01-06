ec2aea7e019ad818f4f6b4cd550601d9d7dbeb43
Denis Wirries <denis@wirries.me>
2019-01-06 07:54:21 +0100

    Change CORS configuration

--- coffeeservice/src/main/java/me/wirries/coffeemonitor/coffeeservice/config/CorsConfiguration.java
+++ coffeeservice/src/main/java/me/wirries/coffeemonitor/coffeeservice/config/CorsConfiguration.java
@@ -1,10 +1,13 @@
 package me.wirries.coffeemonitor.coffeeservice.config;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * This is the CORS configuration of the application. Other settings are in the application.yml.
@@ -16,12 +19,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class CorsConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfiguration.class);
+
+    @Value("${app.cors.allowedOrigins}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/*"); // uncomment only from .allowedOrigins("http://localhost");
+                if (StringUtils.isNotBlank(allowedOrigins)) {
+                    LOGGER.info("Enable CORS - allowed origins only from: " + allowedOrigins);
+                    registry.addMapping("/api/*").allowedOrigins(allowedOrigins);
+                } else {
+                    registry.addMapping("/api/*");
+                }
             }
         };
     }
